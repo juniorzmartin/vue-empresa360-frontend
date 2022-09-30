8a12886e8b7fdab379eab19b09ab7c73ecf59818
Ednilson Martins <juniorzmartin@gmail.com>
2022-09-30 07:49:40 -0300

    Rotas alinhadas (rotas filhas)

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Home from '@/views/Home.vue'
 import Login from '@/views/Login.vue'
 import Site from '@/views/Site.vue'
+import Vendas from '@/components/vendas/Vendas.vue'
+import Servicos from '@/components/servicos/Servicos.vue'
 import { createRouter, createWebHistory} from 'vue-router'
 //createWebHashHistory
 const routes = [
@@ -10,7 +12,11 @@ const routes = [
     },
     {
         path: '/home', //localhost:8080/home
-        component: Home
+        component: Home,
+        children: [
+                { path: 'vendas', component: Vendas}, //localhost:8080/home/vendas
+                { path: 'servicos', component: Servicos} //ocalhost:8080/home/servicos
+        ]
     },
     {
         path: '/login', //localhost:8080/login
