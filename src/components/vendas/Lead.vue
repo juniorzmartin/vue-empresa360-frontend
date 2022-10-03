908d9a4fab4f1727c28e8eab44baaf2a28a516ce
Ednilson Martins <juniorzmartin@gmail.com>
2022-10-03 11:20:02 -0300

    Parâmetros de rotas - Recebendo parâmetros via .params

--- src/components/vendas/Lead.vue
+++ src/components/vendas/Lead.vue
@@ -1,3 +1,14 @@
 <template>
-    <h5>Detalhes da Lead</h5>
+    <h5>Detalhes da Lead {{$route.params.id}}</h5>
 </template>
+
+<script>
+export default {
+    name: 'Lead',
+    /*
+    created() {
+        console.log(this.$route.params)
+    }
+    */
+}
+</script>
